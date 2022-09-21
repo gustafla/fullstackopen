2e8b129bd1546d0f83d906139e11e7903f2fce42
Lauri Gustafsson <me@gustafla.space>
2022-09-21 18:14:54 +0300

    Add osa2

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,58 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.course.parts.map(p => <Part part={p} key={p.id}></Part>)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.course.parts.map(p => p.exercises).reduce((sum, cur) => sum += cur, 0)}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content course={course}></Content>
+      <Total course={course}></Total>
+    </div>
+  )
+}
+
+export default App;
