c8e70a3b188596809d867e90b5f1e6df695732f8
Lauri Gustafsson <me@gustafla.space>
2022-09-27 17:44:13 +0300

    Add osa2/puhelinluettelo in state 2.8

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const ContactList = ({ persons }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          persons.map(person =>
+            <tr key={person.name}>
+              <td>{person.name}</td>
+              <td>{person.number}</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1231244' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form >
+      <h2>Numbers</h2>
+      <ContactList persons={persons} />
+    </div >
+  )
+
+}
+
+export default App
