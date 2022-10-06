eb04ddad0ae0f8e720fa09f47c83642e23e43191
Lauri Gustafsson <me@gustafla.space>
2022-10-06 12:02:00 +0300

    osa4/blogilista: add utils/totalLikes

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (_blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.map(b => b.likes).reduce((a, b) => a + b)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
