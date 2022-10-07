92da5f6ccd3ad42c2e28d8db85667aadaefbf1e9
Lauri Gustafsson <me@gustafla.space>
2022-10-07 10:35:51 +0300

    osa4/blogilista: refactor mostBlogs and mostLikes

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,34 +10,29 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((a, b) => a.likes > b.likes ? a : b)
 }
 
-const mostBlogs = (blogs) => {
+const reduceMaxByAuthor = (blogs, count) => {
   return blogs.reduce((list, blog) => {
     const author_element = list.find(e => e.author === blog.author)
     if (author_element) {
       console.log('found', author_element)
-      author_element.blogs += 1
+      author_element.count += count(blog)
       return list
     } else {
-      const new_element = { author: blog.author, blogs: 1 }
+      const new_element = { author: blog.author, count: count(blog) }
       console.log('creating', new_element)
       return list.concat(new_element)
     }
-  }, []).reduce((a, b) => a.blogs > b.blogs ? a : b)
+  }, []).reduce((a, b) => a.count > b.count ? a : b)
+}
+
+const mostBlogs = (blogs) => {
+  const o = reduceMaxByAuthor(blogs, () => 1)
+  return { author: o.author, blogs: o.count }
 }
 
 const mostLikes = (blogs) => {
-  return blogs.reduce((list, blog) => {
-    const author_element = list.find(e => e.author === blog.author)
-    if (author_element) {
-      console.log('found', author_element)
-      author_element.likes += blog.likes
-      return list
-    } else {
-      const new_element = { author: blog.author, likes: blog.likes }
-      console.log('creating', new_element)
-      return list.concat(new_element)
-    }
-  }, []).reduce((a, b) => a.likes > b.likes ? a : b)
+  const o = reduceMaxByAuthor(blogs, blog => blog.likes)
+  return { author: o.author, likes: o.count }
 }
 
 module.exports = {
