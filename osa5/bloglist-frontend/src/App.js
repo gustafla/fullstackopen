df3607b5bb8e52f336ea161fb8cd38c478f450d9
Lauri Gustafsson <me@gustafla.space>
2022-10-10 14:02:46 +0300

    osa5/bloglist-frontend: Add basic login functionality

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,24 +1,96 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
-const App = () => {
-  const [blogs, setBlogs] = useState([])
+const Login = ({ handleLogin }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    if (await handleLogin(username, password)) {
+      setUsername('')
+      setPassword('')
+    } else {
+      alert('Username or password incorrect')
+    }
+  }
 
   return (
     <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          username
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+const Blogs = ({ user, blogs }) => {
+  return (
+    <div>
+      <p>{user.name ? user.name : user.username} logged in</p>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
+}
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    if (user) {
+      (async () => {
+        const blogs = await blogService.getAll()
+        setBlogs(blogs)
+      })()
+    }
+  }, [user])
+
+  const handleLogin = async (username, password) => {
+    console.log('logging in', username, password)
+    try {
+      const user = await loginService.login({ username, password })
+      blogService.setToken(user.token)
+      setUser(user)
+      return true
+    } catch (exception) {
+      console.log('login failed', exception)
+    }
+    return false
+  }
+
+  return (
+    <div>
+      {user === null ?
+        <Login handleLogin={handleLogin} />
+        : <Blogs user={user} blogs={blogs} />}
+    </div>
+  )
+
 }
 
 export default App
