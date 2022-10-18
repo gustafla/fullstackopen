f5370ab23bdd8987d5d5e2b60973ebe91652bcec
Lauri Gustafsson <me@gustafla.space>
2022-10-18 13:43:18 +0300

    osa5/bloglist-frontend: add backend testing reset to cypress tests (ex 5.17)

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,10 @@
 describe('Blog ', function() {
-  it('login page can be opened', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:3000')
+  })
+
+  it('login page can be opened', function() {
     cy.contains('Login')
     cy.contains('username')
     cy.contains('password')
