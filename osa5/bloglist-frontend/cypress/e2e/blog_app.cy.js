dcd8504e334bef724ad424389fb4223e9ae8a308
Lauri Gustafsson <me@gustafla.space>
2022-10-18 18:30:49 +0300

    osa5/bloglist-frontend: add cypress test for blog creation (ex 5.19)

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -45,4 +45,35 @@ describe('Blog app', function() {
         .should('have.css', 'color', 'rgb(0, 128, 0)')
     })
   })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username: 'gustafla', password: 'password'
+      }).then(function(response) {
+        localStorage.setItem('loggedUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
+      })
+    })
+
+    it('a blog can be created and seen immediately as well as permanently', function() {
+      cy.contains('new blog').click()
+      cy.contains('new blog').should('not.be.visible')
+      cy.get('.blogTitleInput').type('Hello World')
+      cy.get('.blogAuthorInput').type('Tester')
+      cy.get('.blogUrlInput').type('http://localhost:3000')
+      cy.get('.blogCreateButton').click()
+
+      cy.contains('new blog').should('be.visible')
+      cy.get('.blogTitleInput').should('not.be.visible').and('be.empty')
+      cy.get('.blogAuthorInput').should('not.be.visible').and('be.empty')
+      cy.get('.blogUrlInput').should('not.be.visible').and('be.empty')
+      cy.contains('p', 'Hello World')
+      cy.contains('p', 'Tester')
+
+      cy.visit('http://localhost:3000')
+      cy.contains('Hello World')
+      cy.contains('Tester')
+    })
+  })
 })
