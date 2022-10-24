7850dc4a2816ca70f47d1442cdeca495dcb48da0
Lauri Gustafsson <me@gustafla.space>
2022-10-24 17:31:47 +0300

    osa6/redux-anecdotes: add notification reducer (ex 6.10)

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -10,6 +11,7 @@ const AnecdoteForm = () => {
     if (content) {
       event.target.content.value = ''
       dispatch(createAnecdote(content))
+      dispatch(setNotification('Added'))
     }
   }
 
