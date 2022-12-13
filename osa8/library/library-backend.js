58afba68c9734f4431aed98b39550928ee35e189
Lauri Gustafsson <me@gustafla.space>
2022-12-13 16:22:48 +0200

    osa8/library: start using apollo server express

--- osa8/library/library-backend.js
+++ osa8/library/library-backend.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('apollo-server-express')
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const http = require('http')
 
 const mongoose = require('mongoose')
 const User = require('./models/user')
@@ -19,20 +23,33 @@ mongoose.connect(MONGODB_URI).then(() => {
   console.error('error connecting to mongodb:', error.message)
 })
 
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.toLocaleLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
-      const currentUser = await User.findById(decodedToken.id)
-      return { currentUser }
-    }
-  }
-})
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => {
+      const auth = req ? req.headers.authorization : null
+      if (auth && auth.toLocaleLowerCase().startsWith('bearer ')) {
+        const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+        const currentUser = await User.findById(decodedToken.id)
+        return { currentUser }
+      }
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  await server.start()
+
+  server.applyMiddleware({ app, path: '/' })
+
+  const PORT = 4000
+  httpServer.listen(PORT, () => {
+    console.log(`Server ready at port ${PORT}`)
+  })
+}
+
+start()
