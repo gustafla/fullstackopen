3e9db8ced655b59282571441dc17e4ba8a166102
Lauri Gustafsson <me@gustafla.space>
2022-12-14 18:07:04 +0200

    osa9: add exerciseCalculator (ex 9.2)

---
+++ osa9/ex1-7/exerciseCalculator.ts
@@ -0,0 +1,56 @@
+type Rating = 1 | 2 | 3
+type RatingDescription = 'more exercise is required' | 'not too bad but could be better' | 'well done'
+type Hours = number
+
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: Rating,
+  ratingDescription: RatingDescription,
+  target: Hours,
+  average: Hours,
+}
+
+const isNonNegativeReal = (number: number): boolean => {
+  return !isNaN(number) && isFinite(number) && number >= 0
+}
+
+const describeRating = (rating: Rating): RatingDescription => {
+  if (rating === 1) {
+    return 'more exercise is required'
+  }
+  if (rating === 2) {
+    return 'not too bad but could be better'
+  }
+  return 'well done'
+}
+
+const calculateExercises = (dailyHours: Hours[], target: Hours): Result | undefined => {
+  const periodLength = dailyHours.length
+
+  if (periodLength === 0) {
+    return undefined
+  }
+
+  for (const hours of [...dailyHours, target]) {
+    if (!isNonNegativeReal(hours)) {
+      return undefined
+    }
+  }
+
+  const average = dailyHours.reduce((sum, h) => sum + h, 0) / periodLength
+  const rating: Rating = average < target - 0.5 ? 1 : average < target ? 2 : 3
+
+  return ({
+    periodLength,
+    trainingDays: dailyHours.filter((h) => h > 0).length,
+    success: average >= target,
+    rating,
+    ratingDescription: describeRating(rating),
+    target,
+    average,
+  })
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
