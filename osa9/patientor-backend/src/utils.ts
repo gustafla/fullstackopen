c558b6aa7a547f7998e67b07eba98735b5251315
Lauri Gustafsson <me@gustafla.space>
2023-01-25 19:50:32 +0200

    Add patient entries to backend (ex 9.19)

--- osa9/patientor-backend/src/utils.ts
+++ osa9/patientor-backend/src/utils.ts
@@ -1,4 +1,9 @@
-import { NewPatient, Gender } from './types';
+import { NewPatient, Gender, Entry } from './types';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isIterable = (field: any): field is Iterable<unknown> => {
+  return Symbol.iterator in field;
+};
 
 const isString = (field: unknown): field is string => {
   return typeof field === 'string' || field instanceof String;
@@ -46,15 +51,39 @@ const parseGender = (field: unknown, fieldName: string): Gender => {
   return string;
 };
 
+const parseEntries = (field: unknown, fieldName: string): Entry[] => {
+  if (!field) {
+    throw new Error('Missing field: ' + fieldName);
+  }
+
+  if (!isIterable(field)) {
+    throw new Error('Invalid field: ' + fieldName);
+  }
+
+  for (const obj of field) {
+    const entry = obj as Entry | undefined;
+    switch (entry?.type) {
+      case 'Hospital':
+      case 'OccupationalHealthcare':
+      case 'HealthCheck':
+        break;
+      default:
+        throw new Error('Invalid field: ' + fieldName);
+    }
+  }
+
+  return field as Entry[];
+};
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const toNewPatient = ({ name, dateOfBirth, ssn, gender, occupation }: any): NewPatient => {
+export const toNewPatient = ({ name, dateOfBirth, ssn, gender, occupation, entries }: any): NewPatient => {
   const newPatient: NewPatient = {
     name: parseString(name, 'name'),
     dateOfBirth: parseDate(dateOfBirth, 'dateOfBirth'),
     ssn: parseString(ssn, 'ssn'),
     gender: parseGender(gender, 'gender'),
     occupation: parseString(occupation, 'occupation'),
-    entries: [],
+    entries: parseEntries(entries, 'entries'),
   };
 
   return newPatient;
